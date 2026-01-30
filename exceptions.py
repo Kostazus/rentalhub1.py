a1df79bea47cc889a48848bc9b6067f188c15c62
Kostya <bwbw8p24xc@privaterelay.appleid.com>
2026-01-30 14:08:49 +0200

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+class UserNotFoundError(Exception):
+    pass
+
+class ItemNotFoundError(Exception):
+    pass
+
+class ItemNotAvailableError(Exception):
+    pass
+
+class RentalNotFoundError(Exception):
+    pass
+
